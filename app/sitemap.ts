78b7f0b955c1d5d4447474c6a7b63ef81c723285
Tony11081 <chengyadong1112@gmail.com>
2025-08-19 16:48:19 +0800

    1

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { type MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://kkgool.com'
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://kkgool.cc'
   
   return [
     {
